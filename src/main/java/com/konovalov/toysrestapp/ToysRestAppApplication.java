4a051395f85ae75b12283eb21dc66955ad4bfe28
vladimirbelousovk <belousov.vladimir.k@gmail.com>
2023-10-06 19:05:45 +0300

    FEAT: added mongodb to props

--- src/main/java/com/konovalov/toysrestapp/ToysRestAppApplication.java
+++ src/main/java/com/konovalov/toysrestapp/ToysRestAppApplication.java
@@ -2,8 +2,10 @@ package com.konovalov.toysrestapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class ToysRestAppApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/konovalov/toysrestapp/model/Product.java
+++ src/main/java/com/konovalov/toysrestapp/model/Product.java
@@ -13,7 +13,7 @@ import java.math.BigDecimal;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(value = "product")
+@Document(value = "toys")
 public class Product {
     @Id
     private String id;
