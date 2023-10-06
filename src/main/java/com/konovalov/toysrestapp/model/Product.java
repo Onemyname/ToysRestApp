6191b854f5fd222570e0c99b1c94440f277c54cd
vladimirbelousovk <belousov.vladimir.k@gmail.com>
2023-10-06 19:12:28 +0300

    FIX: mongodb value -> collection

--- src/main/java/com/konovalov/toysrestapp/model/Product.java
+++ src/main/java/com/konovalov/toysrestapp/model/Product.java
@@ -13,7 +13,7 @@ import java.math.BigDecimal;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(value = "toys")
+@Document(collection = "toys")
 public class Product {
     @Id
     private String id;
