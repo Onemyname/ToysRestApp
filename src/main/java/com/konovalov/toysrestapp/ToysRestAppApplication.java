b385153e96f78c4fcf4943a1fa98b18f632eff87
Onemyname <konvvadim@yandex.ru>
2023-10-06 15:53:43 +0400

    INIT: create project

---
+++ src/main/java/com/konovalov/toysrestapp/ToysRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.konovalov.toysrestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToysRestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToysRestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/konovalov/toysrestapp/ToysRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konovalov.toysrestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToysRestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
